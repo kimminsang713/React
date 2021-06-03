e9a9f85e076ce19e45ae8e3707f439f02ca9485c
kimminsang <kimminsang713@gmail.com>
2021-06-03 17:57:23 +0900

    blog add

---
+++ react-blog/src/components/pages/writePage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Editor from "../write/editor";
+import Responsive from "../commons/responsive";
+import Header from "../commons/header";
+
+const WritePage =()=>{
+    return (
+
+        <Responsive>
+            <Header/>
+            <Editor/>
+        </Responsive>
+    );
+};
+export default WritePage;
